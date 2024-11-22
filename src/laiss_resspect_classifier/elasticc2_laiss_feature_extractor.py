67e39ecef1ad10d0a1cd1dff1887ed0bfde69a62
drewoldag <47493171+drewoldag@users.noreply.github.com>
2024-11-22 15:52:26 -0800

    Adding make_train_test_val to show that feature extraction works with RESSEPECT `fit` function.

--- src/laiss_resspect_classifier/elasticc2_laiss_feature_extractor.py
+++ src/laiss_resspect_classifier/elasticc2_laiss_feature_extractor.py
@@ -1,7 +1,4 @@
 import numpy as np
-import importlib_resources
-import scipy.interpolate as scinterp
-from astropy.table import Table
 import pandas as pd
 from itertools import chain
 
@@ -40,7 +37,7 @@ class Elasticc2LaissFeatureExtractor(LaissFeatureExtractor):
         '7DCD',
     ]
 
-    id_column = "object_id"
+    id_column = "objectid"
     label_column = "sntype"
     non_anomaly_classes = ["Normal"] # i.e. "Normal", "Ia", ...
 
@@ -52,19 +49,25 @@ class Elasticc2LaissFeatureExtractor(LaissFeatureExtractor):
         
     @classmethod
     def get_metadata_header(cls) -> list[str]:
-        return [cls.id_column, cls.label_column, ] # Add other metadata columns here
+        return [cls.id_column, "redshift", cls.label_column, "sncode", "sample"]
 
+    @classmethod
+    def get_features(cls, filters) -> list[str]:
+        return cls._get_features_per_filter(Elasticc2LaissFeatureExtractor.feature_names, filters) + cls._get_host_features()
 
-    def get_features(self) -> list[str]:
-        return self._get_lc_features() + self._get_host_features()
+    @classmethod
+    def get_feature_header(cls, filters) -> list[str]:
+        return Elasticc2LaissFeatureExtractor.get_metadata_header() + Elasticc2LaissFeatureExtractor.get_features(filters)
 
-    def _get_lc_features(self) -> list[str]:
-        return self._get_features_per_filter(self.features, self.filters)
+    @classmethod
+    def _get_lc_features(cls, filters) -> list[str]:
+        return cls._get_features_per_filter(Elasticc2LaissFeatureExtractor.feature_names, filters)
 
-    def _get_host_features(self) -> list[str]:
+    @classmethod
+    def _get_host_features(cls) -> list[str]:
         return Elasticc2LaissFeatureExtractor.host_feature_names + Elasticc2LaissFeatureExtractor.calculated_host_feature_name
 
-    def all_fit(self) -> np.ndarray:
+    def fit_all(self) -> np.ndarray:
 
         # TODO: check first if TOM had lc features
             # if so - no need to extract them again
@@ -74,7 +77,7 @@ class Elasticc2LaissFeatureExtractor(LaissFeatureExtractor):
         lightcurve = self.photometry
         min_obs_count = 4
         _, property_names, _ = create_base_features_class(MAGN_EXTRACTOR, FLUX_EXTRACTOR)
-        
+
         lc_properties_d={}
         for band, names in property_names.items():
             detections = get_detections(lightcurve, band.lower())
@@ -106,18 +109,31 @@ class Elasticc2LaissFeatureExtractor(LaissFeatureExtractor):
 
             for name, value in zip(names, chain(magn_features, flux_features)):
                 lc_properties_d[name] = value
-    
-        laiss_features = lc_properties_d[self._get_lc_features()].values()
+
+        laiss_features = [lc_properties_d[feature_name] for feature_name in self._get_lc_features(self.filters)]
 
         # extract host features
         host_d = {host_feature: self.additional_info[host_feature] for host_feature in Elasticc2LaissFeatureExtractor.host_feature_names}
-        host_df = pd.DataFrame(host_d, index=[0])  
+        host_df = pd.DataFrame(host_d, index=[0])
 
         # calculate additional host features
         for f, g in zip('griz', 'rizy'):
             host_df[f'{f}-{g}'] = host_df[f'hostgal_mag_{f}'] - host_df[f'hostgal_mag_{g}']
         host_df = calc_7DCD(host_df)
-        host_features = np.array(host_df.iloc[0])
+        host_features = np.array(host_df[self._get_host_features()].iloc[0])
 
         #TODO: check that this array has its features in the correct order for the classifier
-        return laiss_features.extend(host_features)
+        laiss_features.extend(host_features)
+        self.features = laiss_features
+        return self.features
+
+    def get_features_to_write(self) -> list:
+        """Returns features list to write."""
+        features_list = [
+            self.id,
+            self.redshift,
+            self.sntype,
+            self.sncode,
+            self.sample]
+        features_list.extend(self.features)
+        return features_list
---
+++ src/laiss_resspect_classifier/make_train_test_val.py
@@ -0,0 +1,241 @@
+import pandas as pd
+from resspect.fit_lightcurves import fit
+from resspect.tom_client import TomClient
+import os
+import numpy as np
+from laiss_resspect_classifier.elasticc2_laiss_feature_extractor import Elasticc2LaissFeatureExtractor
+
+additional_info = [
+        'hostgal_snsep',
+        'hostgal_ellipticity',
+        'hostgal_sqradius',
+        'hostgal_mag_u',
+        'hostgal_mag_g',
+        'hostgal_mag_r',
+        'hostgal_mag_i',
+        'hostgal_mag_z',
+        'hostgal_mag_y',
+        'hostgal_magerr_u',
+        'hostgal_magerr_g',
+        'hostgal_magerr_r',
+        'hostgal_magerr_i',
+        'hostgal_magerr_z',
+        'hostgal_magerr_y',
+    ]
+
+url = os.environ.get('TOM_URL', "https://desc-tom-2.lbl.gov")
+
+#! TODO - remove these hardcoded values
+username = os.environ.get('TOM_USERNAME', 'awoldag')
+passwordfile = os.environ.get('TOM_PASSWORDFILE', '/Users/drew/code/password.txt')
+
+def get_phot(obj_df):
+
+    tom = TomClient(url = url, username = username, passwordfile = passwordfile)
+
+    # get all of the photometry at once
+    ids = obj_df['diaobject_id'].tolist()
+    res = tom.post('db/runsqlquery/',
+                          json={ 'query': 'SELECT diaobject_id, filtername, midpointtai, psflux, psfluxerr'  
+                                ' FROM elasticc2_ppdbdiaforcedsource' 
+                              ' WHERE diaobject_id IN (%s) ORDER BY diaobject_id, filtername, midpointtai;' % (', '.join(str(id) for id in ids)),
+                                'subdict': {} } )
+    all_phot = res.json()['rows']
+    all_phot_df = pd.DataFrame(all_phot)
+    # if you need mag from the arbitrary flux-
+    all_phot_df['mag'] = -2.5*np.log10(all_phot_df['psflux']) + 27.5
+    all_phot_df['magerr'] = 2.5/np.log(10) * all_phot_df['psfluxerr']/all_phot_df['psflux']
+
+    #! Need to send Rob a message to ask that these features be included when querying for hot super nova
+    host_res = tom.post('db/runsqlquery/',
+                          json={ 'query': 'SELECT diaobject_id, hostgal_mag_u, hostgal_mag_g, hostgal_mag_r, hostgal_mag_i, hostgal_mag_z, hostgal_mag_Y, hostgal_magerr_u, hostgal_magerr_g, hostgal_magerr_r, hostgal_magerr_i, hostgal_magerr_z, hostgal_magerr_Y, hostgal_snsep, hostgal_ellipticity, hostgal_sqradius'
+                                ' FROM elasticc2_ppdbdiaobject'
+                              ' WHERE diaobject_id IN (%s) ORDER BY diaobject_id;' % (', '.join(str(id) for id in ids)),
+                                'subdict': {} } )
+    all_host = host_res.json()['rows']
+
+
+    # format into a list of dicts
+    data = []
+    for idx, obj in obj_df.iterrows():
+        phot = all_phot_df[all_phot_df['diaobject_id'] == obj['diaobject_id']]
+
+        phot_d = {}
+        phot_d['objectid'] = int(obj['diaobject_id'])
+        phot_d['sncode'] = int(obj['gentype'])
+        phot_d['redshift'] = obj['zcmb']
+        phot_d['ra'] = obj['ra']
+        phot_d['dec'] = obj['dec']
+        phot_d['photometry'] = phot[['filtername', 'midpointtai', 'psflux', 'psfluxerr', 'mag', 'magerr']].to_dict(orient='list')
+
+        phot_d['photometry']['band'] = phot_d['photometry']['filtername']
+        phot_d['photometry']['mjd'] = phot_d['photometry']['midpointtai']
+        phot_d['photometry']['flux'] = phot_d['photometry']['psflux']
+        phot_d['photometry']['fluxerr'] = phot_d['photometry']['psfluxerr']
+        phot_d['photometry']['mag'] = phot_d['photometry']['mag']
+        phot_d['photometry']['magerr'] = phot_d['photometry']['magerr']
+        del phot_d['photometry']['filtername']
+        del phot_d['photometry']['midpointtai']
+        del phot_d['photometry']['psflux']
+        del phot_d['photometry']['psfluxerr']
+        phot_d = {**phot_d, **all_host[idx]}
+        del phot_d['diaobject_id']
+        data.append(phot_d)
+
+    return data
+
+def validate_objects(objects_to_test):
+    fe = Elasticc2LaissFeatureExtractor()
+    good_objs = []
+
+    for t_obj in objects_to_test:
+        fe.photometry= pd.DataFrame(t_obj['photometry'])
+        fe.id = t_obj['objectid']
+
+        fe.additional_info = {}
+        for info in additional_info:
+            fe.additional_info[info] = t_obj[info]
+
+        res = fe.fit_all()
+        if res:
+            good_objs.append(t_obj)
+
+    return good_objs
+
+def main():
+
+
+    #MAKE INITIAL TRAINING SET 
+    objs = []
+
+    tom = TomClient(url = url, username = username, passwordfile = passwordfile)
+
+    res = tom.post('db/runsqlquery/',
+                            json={ 'query': 'SELECT diaobject_id, gentype, zcmb, peakmjd,' 
+                                ' peakmag_g, ra, dec FROM elasticc2_diaobjecttruth WHERE peakmjd>61300 and peakmjd<61309 and gentype=10 limit 20;', 
+                                'subdict': {}} )
+    objs.extend(res.json()['rows'])
+
+    res = tom.post('db/runsqlquery/',
+                            json={ 'query': 'SELECT diaobject_id, gentype, zcmb, peakmjd,' 
+                                ' peakmag_g, ra, dec FROM elasticc2_diaobjecttruth WHERE peakmjd>61300 and peakmjd<61309 and gentype=21 limit 10;', 
+                                'subdict': {}} )
+    objs.extend(res.json()['rows'])
+
+    res = tom.post('db/runsqlquery/',
+                            json={ 'query': 'SELECT diaobject_id, gentype, zcmb, peakmjd,' 
+                                ' peakmag_g, ra, dec FROM elasticc2_diaobjecttruth WHERE peakmjd>61300 and peakmjd<61309 and gentype=31 limit 10;', 
+                                'subdict': {}} )
+    objs.extend(res.json()['rows'])
+
+    training_objs = get_phot(pd.DataFrame(objs))
+
+    good_objs = validate_objects(training_objs)
+
+    outdir = 'TOM_days_storage'
+
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    fit(
+        good_objs,
+        output_features_file = outdir+'/TOM_training_features',
+        feature_extractor = 'laiss_resspect_classifier.elasticc2_laiss_feature_extractor.Elasticc2LaissFeatureExtractor',
+        filters='ZTF',
+        additional_info=additional_info
+    )
+    data = pd.read_csv('TOM_days_storage/TOM_training_features',index_col=False)
+    data['orig_sample'] = 'train'
+    data["type"] = np.where(data["sncode"] == 10, 'Ia', 'other')
+    data.to_csv('TOM_days_storage/TOM_training_features',index=False)
+
+    #MAKE TEST SET 
+    objs = []
+
+    tom = TomClient(url = url, username = username, passwordfile = passwordfile)
+
+    res = tom.post('db/runsqlquery/',
+                            json={ 'query': 'SELECT diaobject_id, gentype, zcmb, peakmjd,' 
+                                ' peakmag_g, ra, dec FROM elasticc2_diaobjecttruth WHERE peakmjd>61310 and peakmjd<61339 and gentype=10 limit 1000;', 
+                                'subdict': {}} )
+    objs.extend(res.json()['rows'])
+
+    res = tom.post('db/runsqlquery/',
+                            json={ 'query': 'SELECT diaobject_id, gentype, zcmb, peakmjd,' 
+                                ' peakmag_g, ra, dec FROM elasticc2_diaobjecttruth WHERE peakmjd>61310 and peakmjd<61339 and gentype=21 limit 500;', 
+                                'subdict': {}} )
+    objs.extend(res.json()['rows'])
+
+    res = tom.post('db/runsqlquery/',
+                            json={ 'query': 'SELECT diaobject_id, gentype, zcmb, peakmjd,' 
+                                ' peakmag_g, ra, dec FROM elasticc2_diaobjecttruth WHERE peakmjd>61310 and peakmjd<61339 and gentype=31 limit 500;', 
+                                'subdict': {}} )
+    objs.extend(res.json()['rows'])
+
+    test_objs = get_phot(pd.DataFrame(objs))
+    outdir = 'TOM_days_storage'
+
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    good_test_objs = validate_objects(test_objs)
+
+    fit(
+        good_test_objs,
+        output_features_file = outdir+'/TOM_test_features',
+        feature_extractor = 'laiss_resspect_classifier.elasticc2_laiss_feature_extractor.Elasticc2LaissFeatureExtractor',
+        filters='ZTF',
+        additional_info=additional_info
+    )
+    data = pd.read_csv('TOM_days_storage/TOM_test_features',index_col=False)
+    data['orig_sample'] = 'test'
+    data["type"] = np.where(data["sncode"] == 10, 'Ia', 'other')
+    data.to_csv('TOM_days_storage/TOM_test_features',index=False)
+
+    #MAKE VALIDATION SET 
+    objs = []
+
+    tom = TomClient(url = url, username = username, passwordfile = passwordfile)
+
+    res = tom.post('db/runsqlquery/',
+                            json={ 'query': 'SELECT diaobject_id, gentype, zcmb, peakmjd,' 
+                                ' peakmag_g, ra, dec FROM elasticc2_diaobjecttruth WHERE peakmjd>61340 and gentype=10 limit 1000;', 
+                                'subdict': {}} )
+    objs.extend(res.json()['rows'])
+
+    res = tom.post('db/runsqlquery/',
+                            json={ 'query': 'SELECT diaobject_id, gentype, zcmb, peakmjd,' 
+                                ' peakmag_g, ra, dec FROM elasticc2_diaobjecttruth WHERE peakmjd>61340 and gentype=21 limit 500;', 
+                                'subdict': {}} )
+    objs.extend(res.json()['rows'])
+
+    res = tom.post('db/runsqlquery/',
+                            json={ 'query': 'SELECT diaobject_id, gentype, zcmb, peakmjd,' 
+                                ' peakmag_g, ra, dec FROM elasticc2_diaobjecttruth WHERE peakmjd>61340 and gentype=31 limit 500;', 
+                                'subdict': {}} )
+    objs.extend(res.json()['rows'])
+
+    val_objs = get_phot(pd.DataFrame(objs))
+    outdir = 'TOM_days_storage'
+
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    good_val_objs = validate_objects(val_objs)
+
+    fit(
+        good_val_objs,
+        output_features_file = outdir+'/TOM_validation_features',
+        feature_extractor = 'laiss_resspect_classifier.elasticc2_laiss_feature_extractor.Elasticc2LaissFeatureExtractor',
+        filters='ZTF',
+        additional_info=additional_info
+    )
+    data = pd.read_csv('TOM_days_storage/TOM_validation_features',index_col=False)
+    data['orig_sample'] = 'validation'
+    data["type"] = np.where(data["sncode"] == 10, 'Ia', 'other')
+    data.to_csv('TOM_days_storage/TOM_validation_features',index=False)
+
+
+
+if __name__ == '__main__':
+    main()
